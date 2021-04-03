0e6c50d2e9b586fc29a373d50977f63a6ce40355
joel-holanda <joelholanda45@gmail.com>
2021-04-03 14:44:14 -0300

    quiz do herry potter

---
+++ herryAcao.js
@@ -0,0 +1,195 @@
+const perguntas1 = {
+    pergunta: 'Quando Harry Potter nasceu?',
+    alternativaA: '1º de julho, 1980', 
+    alternativaB: '31 de julho, 1980',
+    alternativaC: '1º de agosto, 1980',
+    alternativaD: '31 de agosto, 1980',
+    resposta: 'a'
+};
+const perguntas2 = {
+    pergunta: 'Quem mata Belatriz Lestrange?',
+    alternativaA: 'Gina Weasley', 
+    alternativaB: 'Hermione Granger',
+    alternativaC: 'Ninfadora Tonks',
+    alternativaD: 'Molly Weasley',
+    resposta: 'd'
+};
+const perguntas3 = {
+    pergunta: 'Na cena "Os sete Harry Potter" quem diz onde o verdadeiro Harry está a Voldemort?',
+    alternativaA: 'Severo Snape', 
+    alternativaB: 'Mundungo Fletcher',
+    alternativaC: 'Stanislau Shunpike',
+    alternativaD: 'Ninguém, o próprio Voldemort o vê',
+    resposta: 'c'
+};
+const perguntas4 = {
+    pergunta: 'Quantos filhos Harry tem?',
+    alternativaA: '1 filho', 
+    alternativaB: '2 filhos',
+    alternativaC: '3 filhos',
+    alternativaD: '4 filhos',
+    resposta: 'c'
+};
+const perguntas5 = {
+    pergunta: 'Quais são os primeiros nomes dos filhos de Harry?',
+    alternativaA: 'Tiago, Severo, Gina', 
+    alternativaB: 'Alvo, Tiago, Luna',
+    alternativaC: 'James, Tiago, Lillían',
+    alternativaD: 'Tiago, Alvo, Lillían',
+    resposta: 'd'
+};
+const perguntas6 = {
+    pergunta: 'Com quem Belatriz Lestrange é presa junto?',
+    alternativaA: 'Severo Snape', 
+    alternativaB: 'Pedro Pettigrew',
+    alternativaC: 'Régulo Black',
+    alternativaD: 'Bartolomeu Crouch Junior',
+    resposta: 'd'
+};
+const perguntas7 = {
+    pergunta: 'quanto é 2 + 2?',
+    alternativaA: '2', 
+    alternativaB: '3',
+    alternativaC: '4',
+    alternativaD: '5',
+    resposta: 'c'
+};
+const perguntas8 = {
+    pergunta: 'quanto é 10 + 2?',
+    alternativaA: '12', 
+    alternativaB: '3',
+    alternativaC: '4',
+    alternativaD: '5',
+    resposta: 'a'
+};
+const perguntas9 = {
+    pergunta: 'quanto é 100 + 2?',
+    alternativaA: '12', 
+    alternativaB: '3',
+    alternativaC: '4',
+    alternativaD: '102',
+    resposta: 'd'
+};
+const perguntas10 = {
+    pergunta: 'qual o nome do melhor programador que voce conhece?(kkkkk)',
+    alternativaA: 'Joel Holanda', 
+    alternativaB: 'Silvo Santos',
+    alternativaC: 'Neymar',
+    alternativaD: 'Gugu ',
+    resposta: 'a'
+};
+
+var questao = [perguntas1, perguntas2, perguntas3, perguntas4, perguntas5, perguntas6, perguntas7, perguntas8, perguntas9, perguntas10, ];
+
+var pontos = 0;
+var mostrarPontos = document.getElementById('numeroPontos');
+
+var contador = 0;
+
+var respostaDoUsuario = "";
+var respostaCerta = "";
+
+var botaoComecar = document.getElementById('comecar');
+var texto = document.getElementById('gosta');
+var alterA = document.getElementById('a');
+var alterB = document.getElementById('b');
+var alterC = document.getElementById('c');
+var alterD = document.getElementById('d'); 
+
+var resultado = document.getElementById('resultado'); 
+
+function comecar() {
+    alterA.style.opacity = '1';
+    alterB.style.opacity = '1';
+    alterC.style.opacity = '1';
+    alterD.style.opacity = '1';
+    botaoComecar.style.opacity = '0';
+
+    botaoComecar.style.pointerEvents = 'none';
+    alterA.style.pointerEvents = 'auto';
+    alterB.style.pointerEvents = 'auto';
+    alterC.style.pointerEvents = 'auto';
+    alterD.style.pointerEvents = 'auto';
+
+    texto.innerHTML = questao[contador].pergunta;
+    alterA.innerHTML = questao[contador].alternativaA;
+    alterB.innerHTML = questao[contador].alternativaB;
+    alterC.innerHTML = questao[contador].alternativaC;
+    alterD.innerHTML = questao[contador].alternativaD;
+    mostrarPontos.innerHTML = (pontos);
+
+    respostaCerta = questao[contador].resposta;
+
+}
+
+function verificar() {
+    if (respostaCerta == respostaDoUsuario){
+        pontos += 1;
+        trocar();
+    } else {
+        trocar();
+    }
+}
+function trocar() {
+    if(contador < 9) {
+        contador += 1;
+        texto.innerHTML = questao[contador].pergunta;
+        alterA.innerHTML = questao[contador].alternativaA;
+        alterB.innerHTML = questao[contador].alternativaB;
+        alterC.innerHTML = questao[contador].alternativaC;
+        alterD.innerHTML = questao[contador].alternativaD;
+        mostrarPontos.innerHTML = (pontos);
+        respostaCerta = questao[contador].resposta;        
+    }else if(contador > 9){
+        final();
+    } else {
+        final();
+    }
+
+}
+
+
+function apertaA() {
+    respostaDoUsuario = "a";
+    verificar();
+    console.log(contador);
+}
+function apertaB() {
+    respostaDoUsuario = "b";
+    verificar();
+    console.log(contador);
+}
+function apertaC() {
+    respostaDoUsuario = "c";
+    verificar();
+    console.log(contador);
+}
+function apertaD() {
+    respostaDoUsuario = "d";
+    verificar();
+    console.log(contador);
+}
+console.log(contador);
+
+
+function final() {
+    alterA.style.opacity = '0';
+    alterB.style.opacity = '0';
+    alterC.style.opacity = '0';
+    alterD.style.opacity = '0';
+
+
+    alterA.style.pointerEvents = 'none';
+    alterB.style.pointerEvents = 'none';
+    alterC.style.pointerEvents = 'none';
+    alterD.style.pointerEvents = 'none';
+
+    resultado.style.opacity = '1';
+    resultado.style.pointerEvents = 'auto';
+
+    texto.innerHTML = 'Obrigado por participar do quiz'
+}
+
+function resul() {
+    alert('Ainda irei terminar')
+}
